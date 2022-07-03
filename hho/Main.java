1b559e86c6a1348a1eb08298ce97e41e782877c3
Dani <ddiaz@tactech.cl>
2022-07-03 16:52:11 -0400

    update problem and main itterations

--- hho/Main.java
+++ hho/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			for (int i = 0; i < 30; i++) {
+			for (int i = 0; i < 2; i++) {
 				StdRandom.newSeed();
 				new Swarm().execute();
 			}
